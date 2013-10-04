f9bde6d98a1e94379465caa13eafeddfef1a33c9
peterrham <peterrham@gmail.com>
2013-10-03 22:26:22 -0700

    Update metadata.rb

--- chef_cookbook/metadata.rb
+++ chef_cookbook/metadata.rb
@@ -3,5 +3,4 @@ maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures phpapp'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
